4ca7e694391d9617c375d8f72277699440e5b008
Talita Lambert <tata.csl@gmail.com>
2022-03-11 14:24:54 -0300

    Add files via upload

---
+++ desafio005.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número: '))
+print ('O antecessor de {} é {}'.format(n, n-1))
+print ('O sucessor de {} é {}'.format(n, n+1))
---
+++ desafio007.py
@@ -0,0 +1,3 @@
+n1 = float(input('Digite a sua primeira nota: '))
+n2 = float(input('Digite a sua segunda nota: '))
+print ('Sua nota média é: {}'.format((n1+n2)/2))
